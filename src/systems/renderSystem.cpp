70a960cde9646da345080b65b24d6bb5d15d5d83
Fizzpop302 <chasevanderzwan@yahoo.com>
2022-05-10 16:38:25 -0700

    ramp collisions for new orientations
    edge cases still need to be addressed

--- src/systems/renderSystem.cpp
+++ src/systems/renderSystem.cpp
@@ -95,7 +95,10 @@ mat4 lookDirToMat(vec3 lookDir) {
 void setModelRC(shared_ptr<Program> curS, Transform* tr) {
 	mat4 Trans = glm::translate(glm::mat4(1.0f), tr->pos);
 	mat4 ScaleS = glm::scale(glm::mat4(1.0f), tr->scale);
-	mat4 ctm = Trans * ScaleS * lookDirToMat(tr->lookDir);
+	mat4 RotX = glm::rotate(glm::mat4(1.0f), tr->rotation.x, vec3(1, 0, 0));
+	mat4 RotY = glm::rotate(glm::mat4(1.0f), tr->rotation.y, vec3(0, 1, 0));
+	mat4 RotZ = glm::rotate(glm::mat4(1.0f), tr->rotation.z, vec3(0, 0, 1));
+	mat4 ctm = Trans * lookDirToMat(tr->lookDir) * RotX * RotY * RotZ * ScaleS;
 	glUniformMatrix4fv(curS->getUniform("M"), 1, GL_FALSE, value_ptr(ctm)); 
 }
 
